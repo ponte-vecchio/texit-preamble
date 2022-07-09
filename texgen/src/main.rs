931abe2e1c0dde6e18f7c6bdcbb052045ebf231c
ponte-vecchio <zkogdxdkur@p.monash.edu>
2022-07-09 13:42:37 +1000

    [add] footer fn

--- texgen/src/main.rs
+++ texgen/src/main.rs
@@ -10,9 +10,21 @@ fn _header() -> String {
     hdr_text
 }
 
+fn _footer() -> String {
+    let catcode_revert = "\\makeatother";
+    let colour_maths = "\\everymath\\expandafter{\\the\\everymath\\color{fg}}";
+    let randomise_theme_at_begin = "\\AtBeginDocument{\\randomTheme\\pagecolor{bg}}";
+    let fg_in_all_begins = "\\def\\begin#1{\\endgroup \\begin{#1}\\color{fg}}";
+    let ftr_text = format!("{}\n{}\n{}\n{}\n",
+        catcode_revert, colour_maths, randomise_theme_at_begin, fg_in_all_begins);
+    ftr_text
+}
+
 
 fn main() {
     let hdr = _header();
+    let ftr = _footer();
 
-    println!("{}", hdr);
+    println!("{}\n\n{}",
+        hdr, ftr);
 }
